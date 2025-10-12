3c19058c8dc808188b07ba66cfd0a84faaee07eb
temuriee <critstoggle@gmail.com>
2025-10-13 02:45:35 +0400

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const test = [
+    {
+      link: "Home",
+      to: "/",
+    },
+    {
+      link: "Projects",
+      to: "/Projects",
+    },
+    {
+      link: "Impact",
+      to: "/Impact",
+    },
+    {
+      link: "Locations",
+      to: "/Locations",
+    },
+  ];
+
   return (
     <div>
-      <nav className="max-w-[1000px]">
-        <Link to="/">Home</Link>
-        <Link to="/Projects">Projects</Link>
-        <Link to="/Impact">Impact</Link>
-        <Link to="/Locations">Locations</Link>
+      <nav className="flex flex-row">
+        {test.map((eachElement) => (
+          <Link
+            className="max-w-[1000px] flex flex-row justify-center items-center font-[500] color-[#74768F]"
+            to={eachElement.to}
+          >
+            {eachElement.link}
+          </Link>
+        ))}
       </nav>
     </div>
   );
